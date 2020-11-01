6057b8f82d174fc361afa458146a8d34d9e029b0
seckinceylan <seckin.ceylan1218@gmail.com>
2020-11-01 11:23:43 -0500

    batman has bat mobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("I will got you Joker");
        // yes he will catch him
+        // Batman has cool bat mobile
     }
 }
